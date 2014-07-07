56008af93aae86d47b4b69896961c28c4b063559
Fred Benenson <fred@kickstarter.com>
2014-07-07 14:38:35 -0400

    Some cosmetic team names / cleanup.

--- script/data.rb
+++ script/data.rb
@@ -1,7 +1,7 @@
 require 'rails'
 require 'CSV'
 
-teams = ["Customer Service", "Communications", "Outreach", "Operations", "Product Manager", "Design", "Engineering"]
+teams = ["Community Support", "Communications", "Community", "Operations", "Product Manager", "Design", "Engineering"]
 specialties = ["Frontend", "Backend", "Data", "Finance", "Human Resources", "Legal", "Mobile" ]
 staff = ["Lincoln Kruiboesch", "Sherill Assaf", "Don Greep", "Andera Levenson", "Fred Pickrell", "Felipe Graen", "Scott Swearengen", "Ivory Sheward", "Mauricio Javis", "Maria Givhan", "Rolande Russer", "Cornelius Samrov", "Domitila Keliipaakaua", "Keneth Fliger", "Mickie Robyn", "Margeret Sofer", "Lashandra Kallevig", "Sherron Nealley", "DeAndra Szpak", "Ethan Kuzara", "Damion Gibala", "Parker Derwin", "Roscoe Pyeatt", "Mara Maria", "Ines Jubilee", "Leandro Mittlestadt", "Letty Swarner", "Nakita Allbritten", "Eggimann Tarone", "Stepanie Palevic", "Ramiro Buckmeon", "Valrie Kleftogiannis", "Tenisha Sandate", "Dian Cham", "Carolina Credo", "Colin Rigoni", "Lorrine Langanke", "Suzi Savannah", "Michaela Barella", "Yuriko Rodefer", "Jerald Wolanin", "Idella Siem", "Edra Weisiger", "Collen Molton", "Orlando Horgan", "Scot Collons", "Deshawn Meinhardt", "Brittani Baccus", "Jerrie Covarrubias", "Ina Larde", "Loyd Donofrio", "Jani McGlothian", "Samuel Janis", "Susan Young", "Jennifer Baker", "William Wood", "Elizabeth Diaz", "Helen Ward", "Campbell Russell", "David Graham", "Ruth Bryant", "Patricia Thompson", "Patricia Ford", "Tony Reuteler", "Michaele Zahm", "Florencio Montogomery", "Terese Delung"]
 
